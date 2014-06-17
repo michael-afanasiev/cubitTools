68799ad0cbdf67562864530e4800e852c562074c
michael-afanasiev <michael.afanasiev@erdw.ethz.ch>
2014-06-17 16:45:24 +0200

    Moving to server

--- cubitTools/bin/generateBaseMesh.py
+++ cubitTools/bin/generateBaseMesh.py
@@ -4,6 +4,8 @@ import os
 import sys
 import cubit
 
+unMeshed = True
+
 if ( len(sys.argv) < 5 or sys.argv[1] == '--help' or sys.argv[1] == '-h' ):
   print 'Usage: ./generateBaseMesh -f \n-f base write path (contains /mesh and /geom) \n-p prefix'
   sys.exit()
@@ -27,7 +29,6 @@ for file in os.listdir ( geomPath + 'regions/' ):
   if file.startswith (prefix):
     pass
   else:
-    print "SKIPPED"
     continue
 
   fields      = file.split ('.')
@@ -36,7 +37,35 @@ for file in os.listdir ( geomPath + 'regions/' ):
     
   cubit.cmd  ('open "' + geomPath + 'regions/' + file + '"')    
     
-  cubit.cmd ('mesh volume all')
+  
+  while unMeshed:
+  
+    cubit.cmd ('mesh vol all')
+    unMeshed    = False
+    badVols     = []
+    numVolumes  = cubit.get_volume_count ()
+    numSurfaces = cubit.get_surface_count ()
+    surfIds     = [x for x in range (1, numSurfaces+1)]
+    volIds      = [x for x in range (1, numVolumes+1)]
+    for vol in volIds:
+        if ( cubit.is_meshed ("volume", vol) ):
+          pass
+        else:
+          badVols.append (vol)
+          print "Volume " + str (vol) + " could not mesh. Attempting to reduce mesh size."
+          unMeshed = True
+
+    if unMeshed:
+        cubit.cmd ('del mesh')
+
+    for surf in surfIds:
+      if cubit.get_owning_volume ('surface', surf) in badVols:
+          reqSize = cubit.get_requested_mesh_size ( 'surface', surf)
+          print "Requested size of surface " + str(surf) + " " + str (reqSize) + "\n"
+          cubit.cmd ('surf ' + str (surf) + ' size ' + str (reqSize - 1) )
+          print "New requested size of surface " + str ( cubit.get_requested_mesh_size ( 'surface', surf ) )
+
+  unMeshed = True
 #  cubit.cmd ('unite body all include_mesh')
   cubit.cmd ('set large exodus file on')
   cubit.cmd ('save as "' + geomPath + 'regions_meshed/' + file + '" overwrite')  
--- cubitTools/bin/refineRadialMesh.py
+++ cubitTools/bin/refineRadialMesh.py
@@ -32,32 +32,34 @@ cubit.init ('.')
 print 'Running'
 
 for file in os.listdir ( geomPath + 'regions_meshed/' ):
+
+  if file.startswith ('col000-090.lon000-090'):
   
-  fields      = file.split ('.')
-  radRegion   = fields[2]
-  exoFileName = fields[0] + '.' + fields[1] + '.' + fields[2] + '.' \
-    + fields[3] + '.ex2'
+    fields      = file.split ('.')
+    radRegion   = fields[2]
+    exoFileName = fields[0] + '.' + fields[1] + '.' + fields[2] + '.' \
+      + fields[3] + '.ex2'
 
-  if ( radRegion != reqRegion ):
-    continue
+    if ( radRegion != reqRegion ):
+      continue
     
-  cubit.cmd  ('open "' + geomPath + 'regions_meshed/' + file + '"')
+    cubit.cmd  ('open "' + geomPath + 'regions_meshed/' + file + '"')
        
-  surfs = [i for i in range (1, cubit.get_surface_count()+1)]
+    surfs = [i for i in range (1, cubit.get_surface_count()+1)]
        
-  targets = []
-  for surf in surfs:
-    if ( cubit.get_surface_type (surf) == 'sphere surface' ):
-      targets.append (str ( surf ))
+    targets = []
+    for surf in surfs:
+      if ( cubit.get_surface_type (surf) == 'sphere surface' ):
+        targets.append (str ( surf ))
     
-  for i in range (0, len(targets)-1, 2 ):
-    refineString = 'Refine min_through_thickness ' + numThrough + ' source surface ' + \
-      targets[i] + ' target surface ' + targets[i+1] + ' anisotropic'
+    for i in range (0, len(targets)-1, 2 ):
+      refineString = 'Refine min_through_thickness ' + numThrough + ' source surface ' + \
+        targets[i] + ' target surface ' + targets[i+1] + ' anisotropic'
      
-    cubit.cmd ( refineString )
+      cubit.cmd ( refineString )
 
-  cubit.cmd ( 'save as "' + geomPath + 'regions_refined/' + file + '" overwrite' )
-  cubit.cmd ( 'export mesh "' + path + 'mesh/refined/' + exoFileName + 
-    '" overwrite' )
+    cubit.cmd ( 'save as "' + geomPath + 'regions_refined/' + file + '" overwrite' )
+    cubit.cmd ( 'export mesh "' + path + 'mesh/refined/' + exoFileName + 
+      '" overwrite' )
  
  
