9b0e70f72a7b12f9a1755643970e8284cce0b826
michael-afanasiev <michael.afanasiev@erdw.ethz.ch>
2014-08-27 09:56:51 +0200

    cleanup

--- drivers/geomDriver.sh
+++ drivers/geomDriver.sh
@@ -1,5 +1,2 @@
-srun ./bin/generateEarthChunks.py -f /mnt/lnec/afanasm/cubitScratch/geom/masters/ -t 6371 -b 6351 -d 1
-srun ./bin/generateEarthChunks.py -f /mnt/lnec/afanasm/cubitScratch/geom/masters/ -t 6351 -b 6271 -d 2
-srun ./bin/generateEarthChunks.py -f /mnt/lnec/afanasm/cubitScratch/geom/masters/ -t 6271 -b 5971 -d 5
-srun ./bin/generateEarthChunks.py -f /mnt/lnec/afanasm/cubitScratch/geom/masters/ -t 5971 -b 5701 -d 5
-srun ./bin/generateEarthChunks.py -f /mnt/lnec/afanasm/cubitScratch/geom/masters/ -t 5701 -b 5371 -d 5
+srun ./bin/generateEarthChunks.py -b 3480 -t 4371 -d 891 -f /mnt/lnec/afanasm/cubitScratch/geom/masters/ 
+sleep 2
