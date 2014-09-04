35bca76691a43afd979f4a8f0c95b26ac31c9e5e
michael-afanasiev <michael.afanasiev@erdw.ethz.ch>
2014-09-04 14:19:07 +0200

    Cleanup and improvements

--- drivers/generateGeomDriver.py
+++ drivers/generateGeomDriver.py
@@ -10,12 +10,8 @@ while switch != 'exit':
   bot  = input ('Enter bottom: ')
   disc = input ('Enter discretization: ')
   size = input ('Input chunk size: ')
-
+              
   for i in range (bot, top, size):
     f.write ('srun ./bin/generateEarthChunks.py -b ' + str (i) + ' -t ' + str (i+size) + ' -d ' + str (disc) + ' -f /mnt/lnec/afanasm/cubitScratch/geom/masters/ \nsleep 2\n')
-
+                      
   switch = raw_input ('Type "exit" to exit, otherwise hit return to add more regions: ')
-
-  
-
-
---
+++ readme.md
@@ -0,0 +1,5 @@
+== cubitTools ==
+
+This collection of tools allows the automatic generation of the mesh used by the comprehensive earth model.
+
+-- more to come --.
