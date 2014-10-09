48cbfd74e3ee5c6868fcd8993f8c0a4295dd8b40
michael-afanasiev <michael.afanasiev@erdw.ethz.ch>
2014-10-09 11:26:14 +0200

    Added external region sizing. Also there are now nodesets and sidesets.

--- bin/addModelChunksToEarth.py
+++ bin/addModelChunksToEarth.py
@@ -3,34 +3,57 @@
 import os
 import sys
 import cubit
+import fileinput
 
+def readParameters ():
+
+  """ Just a little function to read in parameter names and surface sizing functions from a parameter file
+  called regions.txt. Lines with comments are ignored, and a dictionary of region names and sizes is 
+  returned. """
+
+  params = {}
+
+  for line in fileinput.input ('./regions.txt'):
+    
+    if line.startswith ('#') or not line.strip ():
+      continue
+
+    fields = line.split ('=')
+    name, size = fields[0].strip (), float (fields[1])
+    params.update ({name: size})
+
+  return params
+
+# Read parameter file.
+params = readParameters ()
+
+# User specifies where the read directories are.
 if ( len (sys.argv) < 2 or sys.argv[1] == '--help' or sys.argv[1] == '-h' ):
-  sys.exit ('Usage: ./setupMesh -f \n-f base write path (contains /mesh and /geom)')    
+  sys.exit ('Usage: ./addModelChunksToEarth.py -f \n-f base write path (contains /mesh and /geom)')    
 
-overWrite = False
-
+# Read in command line options.
 for i in range (len (sys.argv) - 1 ):
   if sys.argv[i] == '-f':
     basePath = sys.argv[i+1]
 
+# Code looks for a /geom directory to find the earth chunk files (generated in previous step).
 path     = basePath
 geomPath = basePath + 'geom/'
   
+# Initialize cubit.
 cubit.init ('.')
 
+# Here we loop through all the files in the master earth chunk directory
 for file in os.listdir ( geomPath + 'masters/' ):
   
-  fields      = file.split ('.')
-  exoFileName = fields[0] + '.' + fields[1] + '.' + fields[2] + '.' \
-    + fields[3] + '.ex2'    
-  
-  cubit.cmd  ('open "' + geomPath + 'cutters/all_cutters.cub')
 
-  Japan         = False
-  Europe        = False
-  Anatolia      = False
-  Australia     = False
-  SouthAtlantic = False
+  fields      = file.split ('.')
+  
+  cubit.silent_cmd  ('open "' + geomPath + 'cutters/all_cutters.cub')
+
+  paramBool = params.copy ()
+  for key in paramBool.keys ():
+    paramBool[key] = False
 
   names = []
   sizes = []
@@ -38,11 +61,9 @@ for file in os.listdir ( geomPath + 'masters/' ):
   # Set up surface sizing functions.
   fields    = file.split ('.')
   radRegion = fields[2]
-  params = { 'Europe': 50, 'Japan': 50, 'SouthAtlantic': 50, 
-    'Australia': 50, 'Anatolia': 15 }
 
-  cubit.cmd ('import "' + geomPath + 'masters/' + file + '"')    
-  cubit.cmd ('compress all')
+  cubit.silent_cmd ('import "' + geomPath + 'masters/' + file + '"')    
+  cubit.silent_cmd ('compress all')
   numVolumes = cubit.get_volume_count ()
 
   volumeIds   = [x for x in range (1, numVolumes+1)]    
@@ -57,82 +78,75 @@ for file in os.listdir ( geomPath + 'masters/' ):
         vol1 = str (overLapping[i+1])
         vol2 = str (overLapping[i])
 
-      cubit.cmd ('intersect volume ' + vol1 + ' ' + vol2 + ' keep')           
+      cubit.silent_cmd ('intersect volume ' + vol1 + ' ' + vol2 + ' keep')           
   
-  cubit.cmd ('compress all')
+  cubit.silent_cmd ('compress all')
 
+  
   numVolumes  = cubit.get_volume_count ()
   volumeIds   = [x for x in range (1, numVolumes+1)]    
   overLapping = cubit.get_overlapping_volumes ( volumeIds )
   if overLapping:
     for num in overLapping:
-      if cubit.get_entity_name ('volume', num).startswith ('SouthAtlantic'):
-        SouthAtlantic = True
-      if cubit.get_entity_name ('volume', num).startswith ('Australia'):
-        Australia = True          
-      if cubit.get_entity_name ('volume', num).startswith ('Anatolia'):
-        Anatolia = True
-      if cubit.get_entity_name ('volume', num).startswith ('Europe'):
-        Europe = True
-      if cubit.get_entity_name ('volume', num).startswith ('Japan'):
-        Japan = True
-      
-  if Japan:
-    print 'Found Japan'
-    names.append ('Japan')
-    sizes.append (params['Japan'])  
-  if SouthAtlantic:
-    print 'Found SouthAtlantic'
-    names.append ('SouthAtlantic')
-    sizes.append (params['SouthAtlantic'])
-  if Australia:
-    print 'Found Australia'
-    names.append ('Australia')
-    sizes.append (params['Australia'])
-  if Anatolia:
-    print 'Found Anatolia'
-    names.append ('Anatolia')
-    sizes.append (params['Anatolia'])
-  if Europe:
-    print 'Found Europe'
-    names.append ('Europe')
-    sizes.append (params['Europe'])      
-  
+      for key in paramBool.keys ():
+        if cubit.get_entity_name ('volume', num).startswith (key) and key not in names:
+          names.append (key)
+          sizes.append (params[key])
+     
+  # for all named regions, subtract them from the master earth.
   for region in names:
       
-    cubit.cmd ('subtract volume with name "' + region + 
+    cubit.silent_cmd ('subtract volume with name "' + region + 
       '_cutter" from volume with name "masters*"')
 
-  cubit.cmd ('del vol with name "*_cutter')       
+  # delete all the cutters when we're finished with them
+  cubit.silent_cmd ('del vol with name "*_cutter')       
 
-
-  cubit.cmd ( 'compress all' )    
+  # compress namespace.
+  cubit.silent_cmd ( 'compress all' )    
     
-  cubit.cmd ('vol with name "masters*" scheme tetmesh')
-  cubit.cmd ('surf in vol with name "masters*" size 100')                    
-  cubit.cmd ('curve in vol with name "masters*" size 100')
+  # set the sizing functions of the master earth.
+  cubit.silent_cmd ('vol with name "masters*" scheme tetmesh')
+  cubit.silent_cmd ('surf in vol with name "masters*" size 100')                    
+  cubit.silent_cmd ('curve in vol with name "masters*" size 100')
 
+  # go and apply the sizing functions to the proper volumes.
   if names:
     for region, size in zip (names, sizes):
-      cubit.cmd ('vol with name "' + region + '*" scheme tetmesh')
-      cubit.cmd ('surf in vol with name "' + region + '*" size ' + 
+      cubit.silent_cmd ('vol with name "' + region + '*" scheme tetmesh')
+      cubit.silent_cmd ('surf in vol with name "' + region + '*" size ' + 
         str (size) )
-      cubit.cmd ('curve in vol with name "' + region + '*" size ' +
+      cubit.silent_cmd ('curve in vol with name "' + region + '*" size ' +
         str (size) )        
-        
+ 
+  # name element blocks for master earth...
   blockCount = 1
-  cubit.cmd ('block ' + str (blockCount) + ' volume with name "masters*"')
-  cubit.cmd ('block ' + str (blockCount) + ' name "master"')
+  cubit.silent_cmd ('block ' + str (blockCount) + ' volume with name "masters*"')
+  cubit.silent_cmd ('block ' + str (blockCount) + ' name "master"')
+ 
+  # and nodesets...
+  cubit.silent_cmd ('Nodeset ' + str (blockCount) + ' volume with name "masters*"')
+  cubit.silent_cmd ('Nodeset ' + str (blockCount) + ' name "master"')
+
+  # now go through and group the rest of the regions into blocks and nodesets
   blockCount = 2
   for region in names:
-    cubit.cmd ('block ' + str (blockCount) + ' volume with name "' + region + '*"')
-    cubit.cmd ('block ' + str (blockCount) + ' name "' + region + '"')
+
+    cubit.silent_cmd ('block ' + str (blockCount) + ' volume with name "' + region + '*"')
+    cubit.silent_cmd ('block ' + str (blockCount) + ' name "' + region + '"')
+
+    cubit.silent_cmd ('Nodeset ' + str (blockCount) + ' volume with name "' + region + '*"')
+    cubit.silent_cmd ('Nodeset ' + str (blockCount) + ' name "' + region + '"')
+
     blockCount = blockCount + 1
 
 
-  cubit.cmd ('save as "' + geomPath + 'regions/' + file + '" overwrite')  
- 
-  cubit.cmd ('reset')         
+  # Get some badass sidesets
+  cubit.cmd ( 'Sideset 1 surface in volume all with not is_merged' )
+
+  # write and reset for next file.
+  cubit.silent_cmd ('save as "' + geomPath + 'regions/' + file + '" overwrite')  
+  cubit.silent_cmd ('reset')         
 
 if cubit.get_error_count () != 0:
   print "Unfortunately, I found some errors"
